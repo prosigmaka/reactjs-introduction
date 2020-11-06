4eda43bcd849db5480550f71a5e20e907ae86577
Dian Sasmita <dianssmt@gmail.com>
2020-11-06 14:18:34 +0700

    simple form using function

---
+++ simple-form-using-function/src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+
+function App() {
+    return (
+        <form>
+            <label>
+                Name:
+                </label>
+            <input
+                type="text"
+                placeholder="insert name"
+            />
+            <label>
+                Gender:
+                </label>
+            <label className="radio">
+                <input
+                    type="radio"
+                    value="Male"
+                    name="gender"
+                />
+                    Male
+                </label>
+            <label className="radio">
+                <input
+                    type="radio"
+                    value="Female"
+                    name="gender"
+                />
+                    Female
+                </label>
+
+            <label>
+                Birthdate:
+                </label>
+            <input
+                type="date"
+            />
+
+            <label>
+                Address:
+                </label>
+            <textarea
+                rows="3"
+                placeholder="insert address"
+            />
+
+            <label>
+                Religion:
+                </label>
+            <select>
+                <option>Select one</option>
+                <option>Islam</option>
+                <option>Protestant</option>
+                <option>Catholic</option>
+                <option>Hindu</option>
+                <option>Buddha</option>
+                <option>Konghucu</option>
+            </select>
+
+            <input
+                type="submit"
+                value="Submit"
+            />
+        </form >
+    );
+}
+
+export default App;
