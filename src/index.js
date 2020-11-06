dc95fc087efe7b53e40d82fb3464b14a7b6c04a2
Dian Sasmita <dianssmt@gmail.com>
2020-11-06 09:59:48 +0700

    Hello, World!

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <h1>Hello, World!</h1>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
